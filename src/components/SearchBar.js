cd26cf4f76078342bdb9a65c9e07882a575b2005
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-04-24 13:11:20 -0700

    added search bar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+function SearchBar(props) {
+  // the props list includes handleformsubmit, handleInputChange, apple
+  console.log(props.apple) // which would return the string "apple"
+  return (
+    <div className="container-fluid">
+      <form className="d-flex">
+        <input
+          className="form-control me-2"
+          type="search"
+          placeholder="Search"
+          aria-label="Search"
+          value={props.search}
+          onChange={props.handleInputChange}
+          
+        />
+        <datalist id="employees">
+          {props.employees.map(employee => (
+            <option value={employee} key={employee} />
+          ))};
+        </datalist>
+        <button
+          type="submit"
+          onClick={props.handleFormSubmit}
+          className="btn btn-outline-success"
+        >
+          Search
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
