bc4c477489acba476f1a974177183c0af694026f
Stephanie Zavala <stephaniemzavala@gmail.com>
2021-04-26 09:08:17 -0700

    added header and styling

---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./index.css";
+
+function Header() {
+    return (
+        <div className="container-fluid">
+            <h1 className="row align-items-center">Directory</h1>
+            <p>Use the search box to find a company employee. You can search their first name, last name, phone number, email address, or their gender.</p>
+        </div>
+    )
+}
+
+export default Header
